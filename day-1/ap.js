14dcfdde4bb8429532b06edd35fd37a5bb7eb903
andyish <isherwood88@gmail.com>
2018-12-10 21:59:45 +0000

    day 1 part 2

--- day-1/ap.js
+++ day-1/ap.js
@@ -5,7 +5,21 @@ const fs = Promise.promisifyAll(require("fs"));
 const data = fs.readFileSync('./day-1/data', 'utf8')
 dataArray = data.toString().split('\n').map((s) => s.replace('\r', ''))
 
+let value = 0;
+let frequencyMap = {
+    0: 1
+};
 
-const answer = eval(dataArray.join(' '))
+function calculateEmegencyFrequency() {
+    dataArray.forEach(e => {
+        value = eval(`${value} ${e}`)
+        frequencyMap[value] = (frequencyMap[value] || 0) + 1
+    
+        if (frequencyMap[value] === 2) {
+            console.log(value)
+            exit;
+        }
+    });
+}
 
-console.log(answer)
+calculateEmegencyFrequency()
